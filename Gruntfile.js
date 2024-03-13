b930eb085e901eeae855d24b050d6add56e6a161
Kyle <sully8437@gmail.com>
2024-03-13 09:28:44 -0500

    image gallery project init

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    sass: {
+      dist: {
+        files: {
+          'dist/css/styles.css': 'src/scss/styles.scss'
+        }
+      }
+    },
+    
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'dist/js/script.js': ['src/js/script.js']
+        }
+      }
+    },
+    
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/',
+        src: 'index.html',
+        dest: 'dist/',
+      },
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/js/*.js', 'src/scss/*.scss', 'src/*.html'],
+        tasks: ['uglify', 'sass', 'copy'],
+        options: {
+          spawn: false,
+        },
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['uglify', 'sass', 'copy', 'watch']);
+};
---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const imageUrls = [
+    "https://acre-image-collections.s3.amazonaws.com/image-6578722caea701702392364_medium.jpg",
+    "https://acre-images.s3.amazonaws.com/image-5ac62b9e561041522936734_medium.jpeg",
+    "https://acre-images.s3.amazonaws.com/image-5acac631504581523238449_medium.jpeg",
+    "https://acre-image-collections.s3.amazonaws.com/image-65787222618211702392354_medium.jpg",
+    "https://acre-image-collections.s3.amazonaws.com/image-657872175b22e1702392343_medium.jpg",
+    "https://acre-image-collections.s3.amazonaws.com/image-657871ff7d8401702392319_medium.jpg",
+    "https://acre-image-collections.s3.amazonaws.com/image-6576286c7c4b61702242412_medium.jpg",
+    "https://acre-image-collections.s3.amazonaws.com/image-6578af5f9af671702408031_medium.jpg",
+    "https://acre-images.s3.amazonaws.com/image-5b01cdf4b2e9a1526844916.jpg"
+];
+
+    const totalImages = 20;
+    const gallery = document.getElementById('imageGallery');
+
+    for (let index = 0; index < totalImages; index++) {
+        const url = imageUrls[index % imageUrls.length];
+        console.log(url);
+        const col = document.createElement('div');
+        col.className = 'col-6 col-md-4 col-lg-3 image';
+        col.innerHTML = `
+            <label class="image-checkbox" for="checkbox-${index}">
+                <input type="checkbox" id="checkbox-${index}" tabindex="${0}" />
+                <div class="image-background" data-src="${url}"></div>
+            </label>
+        `;
+        gallery.appendChild(col);
+    }
+
+  let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const image = entry.target;
+              image.style.backgroundImage = `url('${image.getAttribute('data-src')}')`;
+              observer.unobserve(image);
+          }
+      });
+  }, {rootMargin: "0px 0px 50px 0px"});
+
+  document.querySelectorAll('.image-background').forEach(img => {
+      observer.observe(img);
+  });
+});
