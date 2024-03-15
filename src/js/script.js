754e2137ae1e042cdeeb3372a41f71ed0f1713fb
Kyle <sully8437@gmail.com>
2024-03-14 23:06:48 -0500

    set dynamic image height by node width

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const imageUrls = [
-    "https://acre-image-collections.s3.amazonaws.com/image-6578722caea701702392364_medium.jpg",
-    "https://acre-images.s3.amazonaws.com/image-5ac62b9e561041522936734_medium.jpeg",
-    "https://acre-images.s3.amazonaws.com/image-5acac631504581523238449_medium.jpeg",
-    "https://acre-image-collections.s3.amazonaws.com/image-65787222618211702392354_medium.jpg",
-    "https://acre-image-collections.s3.amazonaws.com/image-657872175b22e1702392343_medium.jpg",
-    "https://acre-image-collections.s3.amazonaws.com/image-657871ff7d8401702392319_medium.jpg",
-    "https://acre-image-collections.s3.amazonaws.com/image-6576286c7c4b61702242412_medium.jpg",
-    "https://acre-image-collections.s3.amazonaws.com/image-6578af5f9af671702408031_medium.jpg",
-    "https://acre-images.s3.amazonaws.com/image-5b01cdf4b2e9a1526844916.jpg",
-  ];
+document.addEventListener("DOMContentLoaded", initializeGallery);
+const imageUrls = [
+  "https://acre-image-collections.s3.amazonaws.com/image-6578722caea701702392364_medium.jpg",
+  "https://acre-images.s3.amazonaws.com/image-5ac62b9e561041522936734_medium.jpeg",
+  "https://acre-images.s3.amazonaws.com/image-5acac631504581523238449_medium.jpeg",
+  "https://acre-image-collections.s3.amazonaws.com/image-65787222618211702392354_medium.jpg",
+  "https://acre-image-collections.s3.amazonaws.com/image-657872175b22e1702392343_medium.jpg",
+  "https://acre-image-collections.s3.amazonaws.com/image-657871ff7d8401702392319_medium.jpg",
+  "https://acre-image-collections.s3.amazonaws.com/image-6576286c7c4b61702242412_medium.jpg",
+  "https://acre-image-collections.s3.amazonaws.com/image-6578af5f9af671702408031_medium.jpg",
+  "https://acre-images.s3.amazonaws.com/image-5b01cdf4b2e9a1526844916.jpg",
+];
 
+let imageElements;
+
+function initializeGallery() {
+  const galleryElement = document.getElementById("imageGallery");
+  populateGallery(galleryElement);
+  adjustImageHeightsOnResize();
+  initializeImageObserver();
+}
+
+function populateGallery(galleryElement) {
   const totalImages = 20;
-  const gallery = document.getElementById("imageGallery");
 
   for (let index = 0; index < totalImages; index++) {
-    const url = imageUrls[index % imageUrls.length];
-    const col = document.createElement("div");
-    col.className = "image";
-    col.innerHTML = `
-            <label class="image-checkbox" for="checkbox-${index}">
-                <input type="checkbox" id="checkbox-${index}" tabindex="${0}" />
-                <div class="image-background img-thumbnail" data-src="${url}"></div>
-            </label>
-        `;
-    gallery.appendChild(col);
+    const imageUrl = imageUrls[index % imageUrls.length];
+    const galleryItemHtml = createGalleryItemHtml(index, imageUrl);
+    galleryElement.innerHTML += galleryItemHtml;
   }
+  imageElements = document.querySelectorAll(".image-background");
+}
 
-  const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const image = entry.target;
-          const src = image.getAttribute("data-src");
-          image.style.backgroundImage = `url('${src}')`;
-          observer.unobserve(image);
-        }
-      });
-    },
-    { rootMargin: "0px 0px 50px 0px" },
+function createGalleryItemHtml(index, imageUrl) {
+  return `
+      <div class="image">
+        <label class="image-checkbox" for="checkbox-${index}">
+          <input type="checkbox" id="checkbox-${index}" tabindex="0" />
+          <div class="image-background img-thumbnail" data-src="${imageUrl}"></div>
+        </label>
+      </div>
+    `;
+}
+
+function initializeImageObserver() {
+  const observer = new IntersectionObserver(handleIntersection);
+  imageElements.forEach((image) => observer.observe(image));
+}
+
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const imageElement = entry.target;
+      loadImageBackground(imageElement);
+      adjustImageHeight(imageElement);
+      observer.unobserve(imageElement);
+    }
+  });
+}
+
+function loadImageBackground(imageElement) {
+  const imageUrl = imageElement.getAttribute("data-src");
+  imageElement.style.backgroundImage = `url('${imageUrl}')`;
+}
+
+function adjustImageHeight(imageElement) {
+  const width = imageElement.getBoundingClientRect().width;
+  imageElement.style.setProperty("--image-height", `${width}px`);
+}
+
+function adjustImageHeightsOnResize() {
+  window.addEventListener("resize", () =>
+    imageElements.forEach(adjustImageHeight),
   );
-
-  document
-    .querySelectorAll(".image-background")
-    .forEach((img) => observer.observe(img));
-});
+}
